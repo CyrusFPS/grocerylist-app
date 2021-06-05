af23490ead2ab84c4ab3eb010b40146598e645f4
CyrusFPS <71908978+CyrusFPS@users.noreply.github.com>
2021-06-04 20:05:51 -0400

    Delete reducer.js

--- reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./auth";
-import entitiesReducer from "./entities";
-
-export default combineReducers({
-  entities: entitiesReducer,
-  auth: authReducer,
-});
